05b173336c398b0712be51f10b80def6d5a83e89
Vinicius <viniaraujo.1999@gmail.com>
2022-02-10 09:03:21 -0300

    asp com react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MenuSuperior from './components/MenuSuperior/MenuSuperior';
+import Resumo from './components/Resumo/Resumo';
+
+function App() {
+  return (
+
+    <div>
+      <MenuSuperior />
+      <Resumo/>
+    </div>
+  );
+}
+
+export default App;
